ad0306027f24225d62540130b20442c903328ad3
El Miri Martin <elmiri.martin@gmail.com>
2026-04-13 15:29:49 +0200

    kész

--- src/main/java/com/example/mainSceneController.java
+++ src/main/java/com/example/mainSceneController.java
@@ -2,7 +2,6 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
